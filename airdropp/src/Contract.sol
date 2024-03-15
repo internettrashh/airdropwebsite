01dfbfd023c506d61cd6db2dbbb2e767e3f8bd6d
internet_trash <naiknischal92@gmail.com>
2024-03-15 22:51:07 +0530

    final push

--- airdropp/src/Contract.sol
+++ airdropp/src/Contract.sol
@@ -8,10 +8,9 @@ contract Airdrop is ERC20 {
         _mint(msg.sender, initialSupply);
     }
 
-    function claim() public {
-        require(balanceOf(msg.sender) == 20, "You cannot claim anymore tokens");
-        // Convert 0.1 to an integer by multiplying by 10^decimals
-        uint256 amountToMint = 1 * 10 ** decimals(); // 0.1 * 10^decimals()
-        _mint(msg.sender, amountToMint);
-    }
+   function claim() public {
+    // Convert 0.1 to an integer by multiplying by 10^decimals
+    uint256 amountToMint = 1 * 10 ** decimals(); // 0.1 * 10^decimals()
+    _mint(msg.sender, amountToMint);
+}
 }
